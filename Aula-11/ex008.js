b815237045dab630ccd96335eed56964a9296c12
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-06-10 21:11:37 -0300

    first commit

---
+++ Aula-11/ex008.js
@@ -0,0 +1,8 @@
+var velo = 59
+console.log(`A velocidade do seu carro é ${velo} Km/h`)
+if (velo > 60) { 
+    console.log(`Você esta acima da velocidade permitida, MULTADO!`)
+} else {
+    console.log(`Esta dentro do limite, muito bem!`)
+}
+console.log(`Dirija com cuidado, use sinto de segurança`)
