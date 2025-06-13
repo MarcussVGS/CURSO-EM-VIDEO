c746d032a77c35d67be7afc9607e1057d0cc23d8
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-06-12 21:34:39 -0300

    primeira aula osbre repetições,

---
+++ Aula-13/ambiente.js
@@ -0,0 +1,19 @@
+var contador = 0
+do  {
+    console.log(`Ola, Mundo ${contador}`)
+    contador++
+} while (contador < 6)
+
+/*
+while (contador < 6) {
+    console.log(`Ola, Mundo ${contador}`)
+    contador++
+} 
+
+console.log('Ola, Mundo!')
+console.log('Ola, Mundo!')
+console.log('Ola, Mundo!')
+console.log('Ola, Mundo!')
+console.log('Ola, Mundo!')
+console.log('Ola, Mundo!')
+*/
