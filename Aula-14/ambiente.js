4424786213ced566a424cf792de453e58018a5c9
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-06-12 21:45:26 -0300

    segunda aula sobre repetições

---
+++ Aula-14/ambiente.js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Ola, Mundo ${c}`)
+    c++
+} 
+*/
+console.log('Vai comecar...')
+for (var c = 1; c <= 6; c++) {
+    console.log(`Ola, Mundo ${c}`)
+}
+console.log('Fim!')
